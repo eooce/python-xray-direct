f3f95e5009cedd7c55a3b1730a3dd53d07e78122
eooce <142894633+eooce@users.noreply.github.com>
2023-12-22 16:54:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,65 +1,245 @@
-import sys
+import os
+import shutil
 import subprocess
 import http.server
 import socketserver
+import threading
+import requests
+from flask import Flask
+import json
+import time
+import base64
 
+app = Flask(__name__)
 
-# 定义端口号
-port = 3000
+# Set environment variables
+FILE_PATH = os.environ.get('FILE_PATH', './temp')
+PROJECT_URL = os.environ.get('URL', '') # 填写项目分配的url可实现自动访问，例如：https://www.google.com，留空即不启用该功能
+INTERVAL_SECONDS = int(os.environ.get("TIME", 120))                   # 访问间隔时间，默认120s，单位：秒
+UUID = os.environ.get('UUID', 'abe2f2de-13ae-4f1f-bea5-d6c881ca3888')
+NEZHA_SERVER = os.environ.get('NEZHA_SERVER', 'nz.abcd.com')        # 哪吒3个变量不全不运行
+NEZHA_PORT = os.environ.get('NEZHA_PORT', '5555')                  # 哪吒端口为443时开启tls
+NEZHA_KEY = os.environ.get('NEZHA_KEY', 'MMhhCCI8JfwXMDC0ke')
+DOMAIN = os.environ.get('DOMAIN', 'n1.mcst.io')                 # 分配的域名或反代的域名，不带前缀，例如：
+NAME = os.environ.get('NAME', 'Vls')
+PORT = int(os.environ.get('PORT', 3000))            # http服务端口
+VPORT = int(os.environ.get('VPORT', 443))          # 节点端口,游戏玩具类需改为分配的端口,并关闭节点的tls
 
-# 升级pip到最新版本
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'])
+# Create directory if it doesn't exist
+if not os.path.exists(FILE_PATH):
+    os.makedirs(FILE_PATH)
+    print(f"{FILE_PATH} has been created")
+else:
+    print(f"{FILE_PATH} already exists")
 
-# 定义要执行的Shell命令并赋权）
-shell_command = "chmod +x start.sh && ./start.sh" 
+# Clean old files
+paths_to_delete = ['list.txt','sub.txt', 'swith', 'web']
+for file in paths_to_delete:
+    file_path = os.path.join(FILE_PATH, file)
+    try:
+        os.unlink(file_path)
+        print(f"{file_path} has been deleted")
+    except Exception as e:
+        print(f"Skip Delete {file_path}")
 
-# 执行shell文件
-try:
-    completed_process = subprocess.run(['bash', '-c', shell_command], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
-    print("Command executed successfully.")
-except subprocess.CalledProcessError as e:
-    print(f"Error: {e.returncode}")
-    print(e.stdout)
-    print(e.stderr)
-
-
+# http server
 class MyHandler(http.server.SimpleHTTPRequestHandler):
 
-  def do_GET(self):
-    if self.path == '/':
-      self.send_response(200)
-      self.end_headers()
-      self.wfile.write(b'Hello, world')
-    elif self.path == '/list':
-      try:
-        with open("./list.txt", 'rb') as file:
-          content = file.read()
-        self.send_response(200)
-        self.send_header('Content-Type', 'text/plain; charset=utf-8')
-        self.end_headers()
-        self.wfile.write(content)
-      except FileNotFoundError:
-        self.send_response(500)
-        self.end_headers()
-        self.wfile.write(b'Error reading file')
-    elif self.path == '/sub':
-      try:
-        with open("./sub.txt", 'rb') as file:
-          content = file.read()
-        self.send_response(200)
-        self.send_header('Content-Type', 'text/plain; charset=utf-8')
-        self.end_headers()
-        self.wfile.write(content)
-      except FileNotFoundError:
-        self.send_response(500)
-        self.end_headers()
-        self.wfile.write(b'Error reading file')
+    def log_message(self, format, *args):
+        pass
+
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b'Hello, world')
+        elif self.path == '/sub':
+            try:
+                with open(os.path.join(FILE_PATH, 'sub.txt'), 'rb') as file:
+                    content = file.read()
+                self.send_response(200)
+                self.send_header('Content-Type', 'text/plain; charset=utf-8')
+                self.end_headers()
+                self.wfile.write(content)
+            except FileNotFoundError:
+                self.send_response(500)
+                self.end_headers()
+                self.wfile.write(b'Error reading file')
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b'Not found')
+
+httpd = socketserver.TCPServer(('', PORT), MyHandler)
+server_thread = threading.Thread(target=httpd.serve_forever)
+server_thread.daemon = True
+server_thread.start()
+
+# Generate xr-ay config file
+def generate_config():
+    config = {"log": {"access": "/dev/null", "error": "/dev/null", "loglevel": "none",}, "inbounds": [{"port": VPORT, "protocol": "vless", "settings": {"clients": [{"id": UUID, "flow": "xtls-rprx-vision"}], "decryption": "none", "fallbacks": [{"dest": 3001}, {"path": "/vless", "dest": 3002},],}, "streamSettings": {"network": "tcp",},}, {"port": 3001, "listen": "127.0.0.1", "protocol": "vless", "settings": {"clients": [{"id": UUID}], "decryption": "none"}, "streamSettings": {"network": "ws", "security": "none"}}, {"port": 3002, "listen": "127.0.0.1", "protocol": "vless", "settings": {"clients": [{"id": UUID, "level": 0}], "decryption": "none"}, "streamSettings": {"network": "ws", "security": "none", "wsSettings": {"path": "/vless"}}, "sniffing": {"enabled": True, "destOverride": ["http", "tls", "quic"], "metadataOnly": False}},], "dns": {"servers": ["https+local://8.8.8.8/dns-query"]}, "outbounds": [{"protocol": "freedom"}, {"tag": "WARP", "protocol": "wireguard", "settings": {"secretKey": "YFYOAdbw1bKTHlNNi+aEjBM3BO7unuFC5rOkMRAz9XY=", "address": ["172.16.0.2/32", "2606:4700:110:8a36:df92:102a:9602:fa18/128"], "peers": [{"publicKey": "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=", "allowedIPs": ["0.0.0.0/0", "::/0"], "endpoint": "162.159.193.10:2408"}], "reserved": [78, 135, 76], "mtu": 1280}},], "routing": {"domainStrategy": "AsIs", "rules": [{"type": "field", "domain": ["domain:openai.com", "domain:ai.com"], "outboundTag": "WARP"},]}}
+
+    with open(os.path.join(FILE_PATH, 'config.json'), 'w', encoding='utf-8') as config_file:
+        json.dump(config, config_file, ensure_ascii=False, indent=2)
+
+generate_config()
+
+# Determine system architecture
+def get_system_architecture():
+    arch = os.uname().machine
+    return 'arm' if 'arm' in arch else 'amd'
+
+# Download file
+def download_file(file_name, file_url):
+    file_path = os.path.join(FILE_PATH, file_name)
+    with requests.get(file_url, stream=True) as response, open(file_path, 'wb') as file:
+        shutil.copyfileobj(response.raw, file)
+
+# Download and run files
+def download_files_and_run():
+    architecture = get_system_architecture()
+    files_to_download = get_files_for_architecture(architecture)
+
+    if not files_to_download:
+        print("Can't find a file for the current architecture")
+        return
+
+    for file_info in files_to_download:
+        try:
+            download_file(file_info['file_name'], file_info['file_url'])
+            print(f"Downloaded {file_info['file_name']} successfully")
+        except Exception as e:
+            print(f"Download {file_info['file_name']} failed: {e}")
+
+    # Authorize and run
+    files_to_authorize = ['./swith', './web']
+    authorize_files(files_to_authorize)
+
+    # Run ne-zha
+    NEZHA_TLS = ''
+    if NEZHA_SERVER and NEZHA_PORT and NEZHA_KEY:
+        NEZHA_TLS = '--tls' if NEZHA_PORT == '443' else ''
+        command = f"nohup {FILE_PATH}/swith -s {NEZHA_SERVER}:{NEZHA_PORT} -p {NEZHA_KEY} {NEZHA_TLS} >/dev/null 2>&1 &"
+        try:
+            subprocess.run(command, shell=True, check=True)
+            print('swith is running')
+            subprocess.run('sleep 1', shell=True)  # Wait for 1 second
+        except subprocess.CalledProcessError as e:
+            print(f'swith running error: {e}')
     else:
-      self.send_response(404)
-      self.end_headers()
-      self.wfile.write(b'Not found')
+        print('NEZHA variable is empty, skip running')
+
+    # Run xr-ay
+    command1 = f"nohup {FILE_PATH}/web -c {FILE_PATH}/config.json >/dev/null 2>&1 &"
+    try:
+        subprocess.run(command1, shell=True, check=True)
+        print('web is running')
+        subprocess.run('sleep 1', shell=True)  # Wait for 1 second
+    except subprocess.CalledProcessError as e:
+        print(f'web running error: {e}')
+
+    subprocess.run('sleep 3', shell=True)  # Wait for 3 seconds
+	
+
+# Return file information based on system architecture
+def get_files_for_architecture(architecture):
+    if architecture == 'arm':
+        return [
+            {'file_name': 'swith', 'file_url': 'https://github.com/eooce/test/releases/download/ARM/swith'},
+            {'file_name': 'web', 'file_url': 'https://github.com/eooce/test/releases/download/ARM/web'},
+        ]
+    elif architecture == 'amd':
+        return [
+            {'file_name': 'swith', 'file_url': 'https://github.com/eooce/test/releases/download/bulid/swith'},
+            {'file_name': 'web', 'file_url': 'https://github.com/eooce/test/releases/download/123/web'},
+        ]
+    return []
+
+# Authorize files
+def authorize_files(file_paths):
+    new_permissions = 0o775
+
+    for relative_file_path in file_paths:
+        absolute_file_path = os.path.join(FILE_PATH, relative_file_path)
+        try:
+            os.chmod(absolute_file_path, new_permissions)
+            print(f"Empowerment success for {absolute_file_path}: {oct(new_permissions)}")
+        except Exception as e:
+            print(f"Empowerment failed for {absolute_file_path}: {e}")
 
 
-with socketserver.TCPServer(('', port), MyHandler) as httpd:
-  print(f'Server is running on port {port}')
-  httpd.serve_forever()
+
+# Generate list and sub info
+def generate_links():
+    meta_info = subprocess.run(['curl', '-s', 'https://speed.cloudflare.com/meta'], capture_output=True, text=True)
+    meta_info = meta_info.stdout.split('"')
+    ISP = f"{meta_info[25]}-{meta_info[17]}".replace(' ', '_').strip()
+    time.sleep(2)
+ 
+    list_txt = f"""
+vless://{UUID}@{DOMAIN}:{VPORT}?encryption=none&security=tls&sni={DOMAIN}&type=ws&host={DOMAIN}&path=%2Fvless?ed=2048#{NAME}-{ISP}
+  
+    """
+    
+    with open(os.path.join(FILE_PATH, 'list.txt'), 'w', encoding='utf-8') as list_file:
+        list_file.write(list_txt)
+
+    sub_txt = base64.b64encode(list_txt.encode('utf-8')).decode('utf-8')
+    with open(os.path.join(FILE_PATH, 'sub.txt'), 'w', encoding='utf-8') as sub_file:
+        sub_file.write(sub_txt)
+        
+    try:
+        with open(os.path.join(FILE_PATH, 'sub.txt'), 'rb') as file:
+            sub_content = file.read()
+        print(f"\n{sub_content.decode('utf-8')}")
+    except FileNotFoundError:
+        print(f"sub.txt not found")
+    
+    print(f'{FILE_PATH}/sub.txt saved successfully')
+    time.sleep(20)
+
+    # cleanup files
+    files_to_delete = ['list.txt','config.json']
+    for file_to_delete in files_to_delete:
+        file_path_to_delete = os.path.join(FILE_PATH, file_to_delete)
+        try:
+            os.remove(file_path_to_delete)
+            print(f"{file_path_to_delete} has been deleted")
+        except Exception as e:
+            print(f"Error deleting {file_path_to_delete}: {e}")
+
+    print('\033c', end='')
+    print('App is running')
+    print('Thank you for using this script, enjoy!')
+         
+# Run the callback
+def start_server():
+    download_files_and_run()
+    generate_links()
+start_server()
+
+# auto visit project page
+has_logged_empty_message = False
+
+def visit_project_page():
+    try:
+        if not PROJECT_URL or not INTERVAL_SECONDS:
+            global has_logged_empty_message
+            if not has_logged_empty_message:
+                print("URL or TIME variable is empty, Skipping visit web")
+                has_logged_empty_message = True
+            return
+
+        response = requests.get(PROJECT_URL)
+        response.raise_for_status() 
+
+        # print(f"Visiting project page: {PROJECT_URL}")
+        print("Page visited successfully")
+        print('\033c', end='')
+    except requests.exceptions.RequestException as error:
+        print(f"Error visiting project page: {error}")
+
+if __name__ == "__main__":
+    while True:
+        visit_project_page()
+        time.sleep(INTERVAL_SECONDS)
